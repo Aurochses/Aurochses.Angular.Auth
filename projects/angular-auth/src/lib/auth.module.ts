93757f13d411a517c186f0229d08eeba5efce613
Vladimir Rodchenko <vladimir@aurochses.com>
2018-04-13 17:59:49 +0300

    solve angular-auth project structure

---
+++ projects/angular-auth/src/lib/auth.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { AuthComponent } from './auth.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [
+    AuthComponent
+  ],
+  exports: [
+    AuthComponent
+  ]
+})
+export class AuthModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
+import { AuthModule } from '@aurochses/angular-auth';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]

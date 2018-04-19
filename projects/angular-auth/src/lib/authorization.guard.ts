6c238157f31ec5d00aebc0e669999b6fc052ff77
Vladimir Rodchenko <vladimir@aurochses.com>
2018-04-19 18:36:20 +0300

    solve permission

--- projects/angular-auth/src/lib/authorization.guard.ts
+++ projects/angular-auth/src/lib/authorization.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { AuthenticationSettings } from './models/authentication-settings.model';
 import { AuthorizationService } from './services/authorization.service';
@@ -16,13 +17,19 @@ export class AuthorizationGuard implements CanActivate {
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     if (next.data && next.data.permissions) {
-      if (!this.authorizationService.hasPermission(next.data.permissions)) {
-        window.location.href = this.authenticationSettings.authority + '/forbidden?returnUrl=' + location.origin + state.url;
+      return this.authorizationService.hasPermission(next.data.permissions).pipe(
+        map(
+          (hasPermission: boolean) => {
+            if (hasPermission === true) {
+              return true;
+            } else {
+              window.location.href = this.authenticationSettings.authority + '/forbidden?returnUrl=' + location.origin + state.url;
 
-        return false;
-      }
-
-      return true;
+              return false;
+            }
+          }
+        )
+      );
     }
 
     return true;

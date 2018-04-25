e710eab166ae7f2bd762e510da7036a4d9331f47
Vladimir Rodchenko <vladimir@aurochses.com>
2018-04-25 20:46:08 +0300

    clear env service

--- projects/angular-auth/src/lib/authentication.guard.ts
+++ projects/angular-auth/src/lib/authentication.guard.ts
@@ -5,14 +5,14 @@ import { User } from 'oidc-client';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { EnvironmentService } from './services/environment.service';
+import { Environment } from './models/environment.model';
 import { AuthenticationService } from './services/authentication.service';
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
 
     constructor(
-        private environmentService: EnvironmentService,
+        private environment: Environment,
         private authenticationService: AuthenticationService
     ) { }
 
@@ -21,13 +21,13 @@ export class AuthenticationGuard implements CanActivate {
             map(
                 (isLoggedIn: boolean) => {
                     if (isLoggedIn) {
-                        if (this.environmentService.IsDevelopment()) {
+                        if (this.environment.production === false) {
                             console.log(`Guard App: User already retrieved. Continuing to ${state.url}.`);
                         }
 
                         return true;
                     } else {
-                        if (this.environmentService.IsDevelopment()) {
+                        if (this.environment.production === false) {
                             console.log(`Guard App: User not logged. Redirecting to /login (and then to ${state.url}).`);
                         }
 
--- projects/angular-auth/src/lib/models/environment-config.model.ts
+++
@@ -1,3 +0,0 @@
-export class EnvironmentConfig {
-    production: boolean;
-}

672c4563b1ca72ea7b4c3bd076ed48645c5e2f27
Vladimir Rodchenko <vladimir@aurochses.com>
2018-04-27 02:42:51 +0300

    final refactor

--- projects/angular-auth/src/lib/services/authorization.service.ts
+++ projects/angular-auth/src/lib/services/authorization.service.ts
@@ -3,32 +3,31 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+import { Environment } from '../models/environment.model';
 import { AuthenticationService } from './authentication.service';
 import { UserModel } from '../models/user.model';
 
 @Injectable()
 export class AuthorizationService {
 
-    constructor(private authenticationService: AuthenticationService) { }
+    constructor(private environment: Environment, private authenticationService: AuthenticationService) { }
 
-    hasPermission(permissions: Array<string>): Observable<boolean> {
-        return this.authenticationService.getUser().pipe(
-            map(
-                (user: UserModel) => {
-                    let flag = true;
+    hasPermission(permissions: Array<string>): boolean {
+        if (this.authenticationService.currentUser) {
+            let flag = true;
 
-                    permissions.forEach(
-                        permission => {
-                            if (!user.profile.permission || !(user.profile.permission.indexOf(permission) > -1)) {
-                                flag = false;
-                            }
-                        }
-                    );
-
-                    return flag;
+            permissions.forEach(
+                permission => {
+                    if (!this.authenticationService.currentUser.profile.permission || !(this.authenticationService.currentUser.profile.permission.indexOf(permission) > -1)) {
+                        flag = false;
+                    }
                 }
-            )
-        );
+            );
+
+            return flag;
+        }
+
+        return false;
     }
 
 }

1e6b7e3cd39175b283a3c80d2ade0fae7653baf0
Yauhen Sakovich <yauhen.sakovich@specific-group.com>
2018-09-05 18:44:41 +0300

    fix permission error

--- projects/aurochses/angular-auth/src/lib/guards/authorization.guard.ts
+++ projects/aurochses/angular-auth/src/lib/guards/authorization.guard.ts
@@ -1,39 +1,41 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {Injectable} from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
 
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
-import { Environment } from '../models/environment.model';
-import { AuthenticationSettings } from '../models/authentication-settings.model';
-import { AuthorizationService } from '../services/authorization.service';
+import {Environment} from '../models/environment.model';
+import {AuthenticationSettings} from '../models/authentication-settings.model';
+import {AuthorizationService} from '../services/authorization.service';
+import { UserManager } from 'oidc-client';
 
 @Injectable()
 export class AuthorizationGuard implements CanActivate {
+
+  userManager: UserManager;
 
   constructor(
     private environment: Environment,
     private authenticationSettings: AuthenticationSettings,
     private authorizationService: AuthorizationService
-  ) { }
+  ) { this.userManager = new UserManager(authenticationSettings); }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     if (next.data && next.data.permissions) {
-      if (this.authorizationService.hasPermission(next.data.permissions)) {
-        if (!this.environment.production) {
-          console.log(`AuthorizationGuard: User has permission.`);
+      this.userManager.getUser().then(() => {
+        if (this.authorizationService.hasPermission(next.data.permissions)) {
+          if (!this.environment.production) {
+            console.log(`AuthorizationGuard: User has permission.`);
+          }
+          return true;
+        } else {
+          if (!this.environment.production) {
+            console.log(`AuthorizationGuard: User has no permission. Redirecting to Forbidden page.`);
+          }
+          window.location.href = this.authenticationSettings.authority + '/forbidden?returnUrl=' + location.origin + state.url;
+          return false;
         }
-
-        return true;
-      } else {
-        if (!this.environment.production) {
-          console.log(`AuthorizationGuard: User has no permission. Redirecting to Forbidden page.`);
-        }
-
-        window.location.href = this.authenticationSettings.authority + '/forbidden?returnUrl=' + location.origin + state.url;
-
-        return false;
-      }
+      });
     }
 
     return true;
